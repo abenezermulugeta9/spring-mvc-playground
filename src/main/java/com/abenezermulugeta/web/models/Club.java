7e23d03c2936a1c16b4917261fd64f002da756b8
AbenezerM <abenezermulugeta41@gmail.com>
2023-02-16 23:55:09 -0600

    Add Club class into the models package

---
+++ src/main/java/com/abenezermulugeta/web/models/Club.java
@@ -0,0 +1,30 @@
+package com.abenezermulugeta.web.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "clubs")
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDate createdOn;
+    @UpdateTimestamp
+    private LocalDate updatedOn;
+}
