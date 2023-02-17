e9241a31b07156fdbca04a50c2400051db5b3618
AbenezerM <abenezermulugeta41@gmail.com>
2023-02-16 23:42:10 -0600

    Initial commit - setting up project and database

---
+++ src/test/java/com/abenezermulugeta/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abenezermulugeta.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
